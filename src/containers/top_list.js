1c0719a53c5bd7c1536660e217b3f845c6fca637
Calvin Kwong <ckwong93@gmail.com>
2017-06-10 20:29:24 -0700

    need to edit top_list to show top 10 instead of 1

--- src/containers/top_list.js
+++ src/containers/top_list.js
@@ -16,20 +16,33 @@ class TopList extends Component {
     // const list = _.sortBy(top.Data, 'SortOrder');
     // console.log('list of crypto by rank',list),
     console.log('top 10', top)
-    var lowercase = top[0].name.toLowerCase();
-    var logo = `https://files.coinmarketcap.com/static/img/coins/64x64/${lowercase}.png`
-    return (
-      <tr>
-        <td><img src={logo}></img></td>
-        <td>{top[0].name}</td>
-        <td>{top[0].symbol}</td>
-        <td>${top[0].price_usd}</td>
-        <td>{top[0].percent_change_24h}%</td>
+    // var lowercase = top[0].name.toLowerCase();
+    // var logo = `https://files.coinmarketcap.com/static/img/coins/64x64/${lowercase}.png`
+    // return (
+    //   <tr>
+    //     <td><img src={logo}></img></td>
+    //     <td>{top[0].name}</td>
+    //     <td>{top[0].symbol}</td>
+    //     <td>${top[0].price_usd}</td>
+    //     <td>{top[0].percent_change_24h}%</td>
+    //     {/* <td>{price}</td>
+    //     {percentage > 0 ? (<td style={{color: 'green'}}>{percentage}%</td>) : (<td style={{color: 'red'}}>{percentage}%</td>)} */}
+    //   </tr>
+    // )
 
-        {/* <td>{price}</td>
-        {percentage > 0 ? (<td style={{color: 'green'}}>{percentage}%</td>) : (<td style={{color: 'red'}}>{percentage}%</td>)} */}
-      </tr>
-    )
+    for (var i = 0; i < top.length; i++){
+      var lowercase = top[i].name.toLowerCase();
+      var logo = `https://files.coinmarketcap.com/static/img/coins/64x64/${lowercase}.png`
+      return (
+        <tr>
+          <td><img src={logo}></img></td>
+          <td>{top[i].name}</td>
+          <td>{top[i].symbol}</td>
+          <td>${top[i].price_usd}</td>
+          <td>{top[i].percent_change_24h}%</td>
+        </tr>
+      )
+    }
   }
   render(){
     return(
