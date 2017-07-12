680b163a23e43d635d385f9292bcac4093ed56f0
Calvin Kwong <ckwong93@gmail.com>
2017-07-11 22:04:08 -0700

    remove materialize

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import {Navbar, NavItem} from 'react-materialize';
 
 export default class NavHeader extends Component {
 
