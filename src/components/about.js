838ec8076f972f4983586d8694f600a716ff1214
Calvin Kwong <ckwong93@gmail.com>
2017-07-15 15:39:43 -0700

    made changes to landing page, about section, font awesome

--- src/components/about.js
+++ src/components/about.js
@@ -1,39 +1,70 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import {Icon} from 'react-fa';
+
 
 export default class About extends Component {
 
   render(){
     var stockImg = `https://blockgeeks.com/wp-content/uploads/2017/04/bitcoin-exchange.jpg`;
     return (
-      <section>
-          <h1 className="section-title">What are Cryptocurrencies?</h1>
-          <span className="col-xs-6 col-md-4 margintop">
-            <center><img className="about-img img-rounded" src={stockImg}></img></center>
-          </span>
-          <span className="col-xs-12 col-md-8">
-            <blockquote>
-              Cryptocurrencies are digital assets designed to work as mediums of exchange using cryptography to secure the transactions and to control the creation of additional units of the currency.
-              Cryptocurrencies are a subset of alternative currencies, or specifically of digital currencies. - Wikipedia
-            </blockquote>
-             <br></br>
-             <h5>What does that even mean?</h5>
-             In a nutshell, Cryptocurrencies are a new form of payments that can be used for transactions online. Unlike fiat currencies like Dollars and Euros,
-             digital currencies are decentralized, anonymous, and instant. Without going too much further in detail, this means that these transactions
-             have minimal fees, are untraceable, and transfer much faster than those of traditional currencies.<br></br><br></br>
+      <div className="about-gradient">
+        <h1 className="about-title">What are Cryptocurrencies?</h1>
 
-             The purpose of this site is to provide users with a simple and easy-to-understand resource for cryptocurrency and
-             altcoins investment. Think of this as a starter kit towards jumpstarting your altcoin portfolio! Let's begin with a quick introduction
-             to two of the most popular coins, Bitcoin and Ethereum.
-            <center>
-              <Link to="/bitcoin"><Button className="custom-button" bsStyle="warning" style={{'background-color': '#f5b730', 'border':'none'}}>Bitcoin</Button></Link>
-                &nbsp; &nbsp;
-               <Link to="/ethereum"><Button className="custom-button" bsStyle="primary" style={{'background-color': '#4b4b4b', 'border':'none'}}>Ethereum</Button></Link>
-             </center>
-             <hr/>
-          </span>
-      </section>
+        <container className="about-container">
+            <span className="about-text">
+               In a nutshell, Cryptocurrencies are a new form of payments that can be used for transactions online. Unlike fiat currencies like Dollars and Euros,
+               digital currencies are decentralized, anonymous, and instant. This means that these transactions have minimal fees, are untraceable, and transfer much faster than those of traditional currencies. The purpose of this site is to provide users with a simple and easy-to-understand resource for cryptocurrency and
+               altcoins investment. Think of this as a starter kit towards jumpstarting your altcoin portfolio! Let's begin with a quick introduction
+               to two of the most popular coins, Bitcoin and Ethereum.<br></br><br></br>
+            </span><br></br>
+        </container>
+        <div className="col-md-4"><center><Icon name="spinner" size="5x"/><br></br>asdfasdfasdf is asdfasdfasdf is asdfasdfasdf</center></div>
+        <div className="col-md-4"><center><Icon name="facebook" size="5x"/>/><br></br>sdfasdfasdf is asdfasdfasdf is asdfasdfasdfsdfasdfasdf is asdfasdfasdf is asdfasdfasdf</center></div>
+        <div className="col-md-4"><center><Icon name="google" size="5x"/>/><br></br>sdfasdfasdf is asdfasdfasdf is asdfasdfasdfsdfasdfasdf is asdfasdfasdf is asdfasdfasdf</center></div>
+        <div className="col-md-4"><center><Icon name="spinner" size="5x"/>/><br></br>sdfasdfasdf is asdfasdfasdf is asdfasdfasdfsdfasdfasdf is asdfasdfasdf is asdfasdfasdf</center></div>
+        <div className="col-md-4"><center><Icon name="facebook" size="5x"/>/><br></br>sdfasdfasdf is asdfasdfasdf is asdfasdfasdfsdfasdfasdf is asdfasdfasdf is asdfasdfasdf</center></div>
+        <div className="col-md-4"><center><Icon name="google" size="5x"/>/><br></br>sdfasdfasdf is asdfasdfasdf is asdfasdfasdfsdfasdfasdf is asdfasdfasdf is asdfasdfasdf</center></div>
+
+      </div>
+
     )
   }
 }
+
+{/*
+               Most altcoins are either Bitcoin or Ethereum based. What this means is that they are built using the same technologies as their base,
+               but provide unique services. Each individual altcoin provides a special service or interaction. For example, Golems provides supercomputing processing power,
+               Ripple provides instantaneous bank transactions, and Civic provides Identity Verification and Protection. With thousands of altcoins out there,
+               it's easy to get confused and lost. What we're trying to build here is a platform to learn, track, and understand some of these awesome altcoins!<br></br><br></br>
+              <center>
+                <Link to="/bitcoin"><Button className="custom-button" bsStyle="warning" style={{'background-color': '#f5b730', 'border':'none'}}>Bitcoin</Button></Link>
+                  &nbsp; &nbsp;
+                 <Link to="/ethereum"><Button className="custom-button" bsStyle="primary" style={{'background-color': '#4b4b4b', 'border':'none'}}>Ethereum</Button></Link>
+               </center>
+               <hr/> */}
+
+{/* <div>
+  <h1 className="section-title">What are Cryptocurrencies?</h1>
+  <container className="about-container">
+      <span className="about-image margintop">
+        <center><img className="about-img img-rounded" src={stockImg}></img></center>
+      </span>
+      <span className="about-text">
+         In a nutshell, Cryptocurrencies are a new form of payments that can be used for transactions online. Unlike fiat currencies like Dollars and Euros,
+         digital currencies are decentralized, anonymous, and instant. Without going too much further in detail, this means that these transactions
+         have minimal fees, are untraceable, and transfer much faster than those of traditional currencies.<br></br><br></br>
+
+         The purpose of this site is to provide users with a simple and easy-to-understand resource for cryptocurrency and
+         altcoins investment. Think of this as a starter kit towards jumpstarting your altcoin portfolio! Let's begin with a quick introduction
+         to two of the most popular coins, Bitcoin and Ethereum.
+        <center>
+          <Link to="/bitcoin"><Button className="custom-button" bsStyle="warning" style={{'background-color': '#f5b730', 'border':'none'}}>Bitcoin</Button></Link>
+            &nbsp; &nbsp;
+           <Link to="/ethereum"><Button className="custom-button" bsStyle="primary" style={{'background-color': '#4b4b4b', 'border':'none'}}>Ethereum</Button></Link>
+         </center>
+         <hr/>
+      </span>
+  </container>
+</div> */}
--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,31 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style!css?sourceMap'
+      }, {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      },{
       exclude: /node_modules/,
       loader: 'babel',
       query: {
         presets: ['react', 'es2015', 'stage-1']
-      }
+      },
     }]
   },
   resolve: {
