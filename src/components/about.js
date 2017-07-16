9406068276e5412ea35b92c6873d3fb716e831fb
Calvin Kwong <ckwong93@gmail.com>
2017-07-15 19:14:34 -0700

    add animation to about page

--- src/components/about.js
+++ src/components/about.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import {Icon} from 'react-fa';
-
+import sr from './scrollReveal.js'
 
 export default class About extends Component {
+  props: Props;
 
+    componentDidMount = () => {
+      const config = {
+        origin: 'right',
+        duration: 1000,
+        delay: 150,
+        distance: '500px',
+        scale: 1,
+        easing: 'ease',
+      }
+// scrollreveal function to animate about section + columns
+      sr.reveal('.about-container', config)
+      sr.reveal('.about-col',{ duration: 1000 }, 100)
+    }
   render(){
     var stockImg = `https://blockgeeks.com/wp-content/uploads/2017/04/bitcoin-exchange.jpg`;
     return (
@@ -18,23 +32,23 @@ export default class About extends Component {
                digital currencies are decentralized, anonymous, and instant. This means that these transactions have minimal fees, are untraceable, and transfer much faster than those of traditional currencies.
                <br></br><br></br>
               There are multiple reasons why these alternative currencies are disrupting the world. With a multitude of advantages and an ever-evolving ecosystem, now is the perfect time to
-              dip your toes into cryptocurrency investment. 
-               <br></br><br></br>
+              dip your toes into cryptocurrency investment.
+               <br></br><br></br><hr></hr>
             </span><br></br>
         </container>
-        <div className="col-md-4 about-col"><center><Icon name="lock" size="5x" className="grow about-icon"/><br></br><h4>Secure</h4><p className="column-text">
+        <div className="col-md-4 about-col"><center><Icon name="lock" size="5x" className="grow about-icon" refs="box"/><br></br><h4>Secure</h4><p className="column-text">
         Because each individual user has their own hashed address, it is secure and easy to send payment to others. There are also a wide variety of security
         measures such as hard wallets, e-wallets, and layered authentication. These layers of security protect coin holders from malicious attacks.
         </p></center></div>
-        <div className="col-md-4 about-col"><center><Icon name="exchange" size="5x" className="grow about-icon"/><br></br><h4>Instant</h4><p className="column-text">All transactions via alt coins are sent instantly. There is no need to ever
+        <div className="col-md-4 about-col"><center><Icon name="exchange" size="5x" className="grow about-icon" refs="box"/><br></br><h4>Instant</h4><p className="column-text">All transactions via alt coins are sent instantly. There is no need to ever
           worry about waiting multiple days for money to be sent to and from these mediums. In fact, there are also alt coins that specifically focus on instant transactions with minimal fees (XRP).</p></center></div>
-        <div className="col-md-4 about-col"><center><Icon name="line-chart" size="5x" className="grow about-icon"/><br></br><h4>Growth</h4><p className="column-text">A growing userbase means that there are new investors everyday. Money pooled into the alt coin industry is growing exponentially. What this means is that the value of our coins are growing at an alarming rate. Think stock market…but on steroids.</p></center></div>
-        <div className="col-md-4 about-col"><center><Icon name="users" size="5x" className="grow about-icon"/><br></br><h4>Community</h4><p className="column-text">Because alt coins are based on the internet, its online presence is extremely active. Sources like Reddit, Twitter, and Slack are great resources to chat with fellow investors about investment opportunities and strategies. Plus, most seasoned investors share all this information publicly!</p></center></div>
-        <div className="col-md-4 about-col"><center><Icon name="link" size="5x" className="grow about-icon"/><br></br><h4>Blockchain</h4><p className="column-text">In essence, a distributed ledger technology, Blockchain allows altcoins to be decentralized.
+        <div className="col-md-4 about-col"><center><Icon name="line-chart" size="5x" className="grow about-icon" refs="box"/><br></br><h4>Growth</h4><p className="column-text">A growing userbase means that there are new investors everyday. Money pooled into the alt coin industry is growing exponentially. What this means is that the value of our coins are growing at an alarming rate. Think stock market…but on steroids.</p></center></div>
+        <div className="col-md-4 about-col"><center><Icon name="users" size="5x" className="grow about-icon" refs="box"/><br></br><h4>Community</h4><p className="column-text">Because alt coins are based on the internet, its online presence is extremely active. Sources like Reddit, Twitter, and Slack are great resources to chat with fellow investors about investment opportunities and strategies. Plus, most seasoned investors share all this information publicly!</p></center></div>
+        <div className="col-md-4 about-col"><center><Icon name="link" size="5x" className="grow about-icon" refs="box"/><br></br><h4>Blockchain</h4><p className="column-text">In essence, a distributed ledger technology, Blockchain allows altcoins to be decentralized.
         That is, there are no 3rd party middleman to rack up fees or taxes (aka government). This decentralisation is one of the revolutionary aspects of the technology.</p></center></div>
-        <div className="col-md-4 about-col"><center><Icon name="university" size="5x" className="grow about-icon"/><br></br><h4>Accessibility</h4><p className="column-text">Since coins are online based, all you need to access your currencies is an internet connection. Long gone
+        <div className="col-md-4 about-col"><center><Icon name="university" size="5x" className="grow about-icon" refs="box"/><br></br><h4>Accessibility</h4><p className="column-text">Since coins are online based, all you need to access your currencies is an internet connection. Long gone
         are the days of needing to drive to banking institutions. All coin transactions are sent and received over the web.</p></center></div>
-
+<hr></hr>
       </div>
 
     )
