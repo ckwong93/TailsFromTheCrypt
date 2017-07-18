de0fd17746e7106fc76c62faf03078884a29bc56
Calvin Kwong <ckwong93@gmail.com>
2017-07-17 19:39:32 -0700

    fix typo

--- src/components/search.js
+++ src/components/search.js
@@ -10,7 +10,7 @@ export default class Search extends Component {
     return (
       <div>
         <Nav />
-        <AboutTop />
+        <AboutSearch />
         <SearchBar />
         <CryptoList />
       </div>
